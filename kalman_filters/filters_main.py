83acc702aaf93fcc2d15ed8f22c5748f504b5b31
oleg-desktop <archonoff@gmail.com>
2018-07-01 21:20:57 +0300

    linear kalman filter (worse than ekf)

--- kalman_filters/filters_main.py
+++ kalman_filters/filters_main.py
@@ -1,11 +1,11 @@
-from itertools import product
+from itertools import cycle
 
 from math import pi
 from matplotlib import pyplot as plt
 from matplotlib.ticker import FormatStrFormatter
 
 from kalman_filters.robot import Robot
-from kalman_filters.kalman_filter import ExtendedKalmanFilter, KalmanFilterBase
+from kalman_filters.kalman_filter import ExtendedKalmanFilter, KalmanFilterBase, KalmanFilter
 from kalman_filters.utils import distance_between
 
 
@@ -49,23 +49,55 @@ if __name__ == '__main__':
     measurement_noise = .05 * test_target.distance
     test_target.set_noise(0.0, 0.0, measurement_noise)
 
-    R_multipliers = [10, 100, 1000, 10000]
-    Q_multipliers = [.1, .01, .001, .0001]
+    line_styles = ["-", "--", "--.", "-.", "-..", ":"]
+    line_cycler = cycle(line_styles)
+
+    # R_multipliers = [1, 10, 100, 1000, 10000]
+    # Q_multipliers = [1, .1, .01, .001, .0001]
+    # P_multipliers = [.1, 1, 10, 100, 1000, 10000]
+    R_multipliers = []
+    Q_multipliers = [1, .1, .01, .001, .0001]
+    P_multipliers = [10]
+    exclude = ()
+    (
+        (1, 1),
+        (1, 0.1),
+        (1, 0.001),
+        (1, 0.0001),
+        (10, 1),
+        (10, 0.01),
+        (10, 0.001),
+        (10, 0.0001),
+        (100, 0.1),
+        (100, 0.0001),
+        (1000, 1),
+        (1000, 0.1),
+        (1000, 0.01),
+        (1000, 0.0001),
+        (10000, 0.0001),
+    )
+    # хорошие: (10, 0.1), (100, 0.01), (100, 0.001), (10000, 0.1)
+    # R_multipliers = [10]
+    # Q_multipliers = [.1]
 
     figure, axes = plt.subplots(nrows=2, ncols=1)   #, sharex=True, gridspec_kw={'height_ratios': (5, 1)})
-    figure.set_figheight(10)
-    figure.set_figwidth(8)
+    figure.set_figheight(20)
+    figure.set_figwidth(16)
     ax1, ax2 = axes
 
-    for R_multiplier, Q_multiplier in product(R_multipliers, Q_multipliers):
-        kalman_filter = ExtendedKalmanFilter(R_multiplier=R_multiplier, Q_multiplier=Q_multiplier, P_multiplier=100)
+    # todo показывать типом линии R, а цветом линии Q
+    # choices = [(R, Q, P) for R in R_multipliers for Q in Q_multipliers for P in P_multipliers if (R, Q) not in exclude]
+    choices = ((1, 1, 10), (1, 0.1, 10), (10, 1, 10))
+    for R_multiplier, Q_multiplier, P_multiplier in choices:
+        # kalman_filter = ExtendedKalmanFilter(R_multiplier=R_multiplier, Q_multiplier=Q_multiplier, P_multiplier=100)
+        kalman_filter = KalmanFilter(R_multiplier=R_multiplier, Q_multiplier=Q_multiplier, P_multiplier=100)
         measured_positions, filtered_positions, errors = run_filter(kalman_filter, test_target)
 
-        # ax1.scatter(*zip(*measured_positions), s=2, label='Измеренное положение')
-        # ax1.plot(*zip(*filtered_positions), linewidth=.2, label='Отфильтрованное положение')
-        # ax1.legend()
+        ax1.scatter(*zip(*measured_positions), s=2, label='Измеренное положение')
+        ax1.plot(*zip(*filtered_positions), linewidth=.2, label='Отфильтрованное положение')
+        ax1.legend()
 
-        ax2.plot(errors, label='R multiplier: {}, Q_multiplier: {}'.format(R_multiplier, Q_multiplier))
+        ax2.plot(errors, next(line_cycler), label='R: {}, Q: {}, P: {}'.format(R_multiplier, Q_multiplier, P_multiplier))
 
     ax2.set_yscale('log')
     ax2.yaxis.set_major_formatter(FormatStrFormatter('%g'))
--- kalman_filters/kalman_filter.py
+++ kalman_filters/kalman_filter.py
@@ -3,46 +3,14 @@ from math import *
 
 
 class KalmanFilterBase:
-    def _init_X(self, *args, **kwargs):
-        raise NotImplementedError
+    state_size = NotImplemented
+    measurement_size = NotImplemented
 
-    def _get_F(self, X, dt=None):
-        raise NotImplementedError
-
-    def _get_P(self, X, P_multiplier, dt=None):
-        raise NotImplementedError
-
-    def _get_H(self, X, dt=None):
-        raise NotImplementedError
-
-    def _get_R(self, X, R_multiplier, dt=None):
-        raise NotImplementedError
-
-    def step(self, measurement, dt=1):
-        raise NotImplementedError
-
-    def get_position(self):
-        """Gets last estimated position"""
-        xy_estimate = self.X[0, 0], self.X[1, 0]
-        return xy_estimate
-
-    def get_prediction(self, steps=1, dt=1):
-        raise NotImplementedError
-
-
-class KalmanFilter(KalmanFilterBase):
-        pass
-
-
-class ExtendedKalmanFilter(KalmanFilterBase):
     def __init__(self, measurement=None, R_multiplier=100, Q_multiplier=.001, P_multiplier=1000):
         if measurement is None:
             x_measurement, y_measurement = (0, 0)
         else:
             x_measurement, y_measurement = measurement
-
-        self.state_size = 5
-        self.measurement_size = 2
 
         self.X = self._init_X(x_measurement, y_measurement)
         self.I = np.matrix(np.identity(self.state_size))
@@ -50,6 +18,95 @@ class ExtendedKalmanFilter(KalmanFilterBase):
         self.P = self._get_P(self.X, P_multiplier=P_multiplier)
         self.R = self._get_R(self.X, R_multiplier=R_multiplier)
         self.Q_multiplier = Q_multiplier
+
+    def get_position(self):
+        """Gets last estimated position"""
+        xy_estimate = self.X[0, 0], self.X[1, 0]
+        return xy_estimate
+
+    def _get_P(self, X, P_multiplier, dt=None):
+        P = self.I * P_multiplier
+        return P
+
+    def _get_R(self, X, R_multiplier, dt=None):
+        R = np.matrix(np.identity(self.measurement_size)) * R_multiplier
+        return R
+
+    def _init_X(self, *args, **kwargs):
+        raise NotImplementedError
+
+    def _get_F(self, X, dt=None):
+        raise NotImplementedError
+
+    def _get_H(self, X, dt=None):
+        raise NotImplementedError
+
+    def step(self, measurement, dt=1):
+        raise NotImplementedError
+
+    def get_prediction(self, steps=1, dt=1):
+        raise NotImplementedError
+
+
+class KalmanFilter(KalmanFilterBase):
+    def __init__(self, *args, **kwargs):
+        self.state_size = 6
+        self.measurement_size = 2
+
+        super().__init__(*args, **kwargs)
+
+    def _init_X(self, x_measurement, y_measurement):
+        return np.matrix([x_measurement, y_measurement, 0, 0, 0, 0]).T
+
+    def _get_F(self, X, dt=None):
+        F = np.matrix([
+            [1, 0, dt, 0, dt**2, 0],
+            [0, 1, 0, dt, 0, dt**2],
+            [0, 0, 1, 0, dt, 0],
+            [0, 0, 0, 1, 0, dt],
+            [0, 0, 0, 0, 1, 0],
+            [0, 0, 0, 0, 0, 1],
+        ])
+        return F
+
+    def _get_H(self, X, dt=None):
+        H = np.matrix([[1, 0, 0, 0, 0, 0],
+                       [0, 1, 0, 0, 0, 0]])
+        return H
+
+    def step(self, measurement, dt=1):
+        F = self._get_F(self.X, dt)
+        Q = F * F.T * self.Q_multiplier
+
+        self.X = F * self.X
+        self.P = F * self.P * F.T + Q
+
+        Z = np.matrix(measurement).T
+        Y = Z - self.H * self.X
+        S = self.H * self.P * self.H.T + self.R
+        K = self.P * self.H.T * np.linalg.pinv(S)
+        self.X = self.X + K * Y
+        self.P = (self.I - K * self.H) * self.P
+
+    def get_prediction(self, steps=1, dt=1):
+        """Gets prediction or robot position after given number of steps"""
+        X = self.X
+
+        for step in range(steps):
+            F = self._get_F(X, dt)
+            X = F * X
+
+        xy_estimate = X[0, 0], X[1, 0]
+
+        return xy_estimate
+
+
+class ExtendedKalmanFilter(KalmanFilterBase):
+    def __init__(self, *args, **kwargs):
+        self.state_size = 5
+        self.measurement_size = 2
+
+        super().__init__(*args, **kwargs)
 
     def _unpack_X(self, X):
         x = X[0, 0]
@@ -83,18 +140,10 @@ class ExtendedKalmanFilter(KalmanFilterBase):
 
         return F
 
-    def _get_P(self, X, P_multiplier, dt=None):
-        P = self.I * P_multiplier
-        return P
-
     def _get_H(self, X, dt=None):
         H = np.matrix([[1, 0, 0, 0, 0],
                        [0, 1, 0, 0, 0]])
         return H
-
-    def _get_R(self, X, R_multiplier, dt=None):
-        R = np.matrix(np.identity(self.measurement_size)) * R_multiplier
-        return R
 
     def step(self, measurement, dt=1):
         F = self._get_F(self.X, dt)
