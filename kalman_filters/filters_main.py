7a027c4441212951453f68af43d335cda057b839
oleg-desktop <archonoff@gmail.com>
2018-07-01 18:34:23 +0300

    graph for filter error

--- kalman_filters/filters_main.py
+++ kalman_filters/filters_main.py
@@ -1,5 +1,6 @@
 from math import pi
 from matplotlib import pyplot as plt
+from matplotlib.ticker import FormatStrFormatter
 
 from kalman_filters.robot import Robot
 from kalman_filters.kalman_filter import ExtendedKalmanFilter, KalmanFilterBase
@@ -44,20 +45,30 @@ def run_filter(kalman_filter: KalmanFilterBase, target_bot: Robot):
 
     ax1.scatter(*zip(*measured_positions), s=2, label='Измеренное положение')
     ax1.plot(*zip(*filtered_positions), linewidth=.2, label='Отфильтрованное положение')
+    ax1.legend()
 
     ax2.plot(errors)
+    ax2.set_yscale('log')
+    ax2.yaxis.set_major_formatter(FormatStrFormatter('%g'))
+    ax2.yaxis.set_minor_formatter(FormatStrFormatter('%g'))
+    ax2.tick_params(which='minor', labelsize=8)
+    ax2.grid(which='both')
 
-    [axis.legend() for axis in axes]
     plt.show()
 
     return localized
 
 
 if __name__ == '__main__':
-    test_target = Robot(2.1, 4.3, 0.5, 2 * pi / 34.0, 1.5)
-    measurement_noise = 0.05 * test_target.distance
+    # test_target = Robot(2.1, 4.3, 0.5, 2 * pi / 34.0, 1.5)        # From "Adding noise" exercise
+    test_target = Robot(0.0, 10.0, 0.0, 2*pi / 30, 1.5)             # From "The final hunt" exercise
+
+    measurement_noise = .05 * test_target.distance
     test_target.set_noise(0.0, 0.0, measurement_noise)
 
-    extended_kalman_filter = ExtendedKalmanFilter()
+    mode = 'ekf'
 
-    run_filter(extended_kalman_filter, test_target)
+    if mode == 'ekf':
+        kalman_filter = ExtendedKalmanFilter(R_multiplier=100, Q_multiplier=.0001, P_multiplier=100000)
+
+    run_filter(kalman_filter, test_target)
