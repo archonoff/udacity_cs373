b40c8a84e990b44f68e401759fee0ca4c35f6e87
oleg-mac <o.zaytsev@greenlabs.ru>
2018-07-02 17:10:18 +0300

    some prepare code for pytorch implementation

---
+++ kalman_filters/filters_main_pytorch.py
@@ -0,0 +1,94 @@
+from itertools import cycle
+
+from math import pi
+from matplotlib import pyplot as plt
+from matplotlib.ticker import FormatStrFormatter
+
+from kalman_filters.robot import Robot
+from kalman_filters.kalman_filter import ExtendedKalmanFilter, KalmanFilterBase, KalmanFilter
+from kalman_filters.kalman_filter_pytorch import KalmanFilter as KalmanFiletrPyTorch
+from kalman_filters.utils import distance_between
+
+
+def run_filter(kalman_filter: KalmanFilterBase, target_bot: Robot):
+    localized = False
+    distance_tolerance = 0.01 * target_bot.distance
+    ctr = 0
+
+    true_positions = []
+    measured_positions = []
+    filtered_positions = []
+    errors = []
+
+    while not localized and ctr <= 1000:
+        ctr += 1
+        measurement = target_bot.sense()
+
+        kalman_filter.step(measurement)
+        position_guess = kalman_filter.get_prediction()
+
+        target_bot.move_in_circle()
+        true_position = (target_bot.x, target_bot.y)
+        error = distance_between(position_guess, true_position)
+        errors.append(error)
+
+        if error <= distance_tolerance:
+            print("You got it right! It took you ", ctr, " steps to localize, R: {}, Q: {}, P: {}".format(kalman_filter.R_multiplier, kalman_filter.Q_multiplier, kalman_filter.P_multiplier))
+            # localized = True
+        # if ctr == 1000:
+        #     print("Sorry, it took you too many steps to localize the target.")
+
+        measured_positions.append(measurement)
+        filtered_positions.append(position_guess)
+        true_positions.append(true_position)
+
+    return measured_positions, filtered_positions, errors, true_positions
+
+
+def find_optimal(test_target: Robot):
+    kf = KalmanFiletrPyTorch(test_target)
+
+    errors = []
+    for optimezation_step in range(10):
+        loss = kf.run_filter()
+
+
+if __name__ == '__main__':
+    test_target = Robot(0.0, 10.0, 0.0, 2*pi / 30, 1.5)
+
+    measurement_noise = .05 * test_target.distance
+    test_target.set_noise(0.0, 0.0, measurement_noise)
+
+    find_optimal(test_target)
+    exit()
+
+    line_styles = ["-", "--", "--.", "-.", "-..", ":"]
+    line_cycler = cycle(line_styles)
+
+    # Preparing graph
+    figure, axes = plt.subplots(nrows=2, ncols=1)
+    figure.set_figheight(10)
+    figure.set_figwidth(8)
+    ax1, ax2 = axes
+
+    # Run Kalman filter
+    kalman_filter = KalmanFilter(R_multiplier=100, Q_multiplier=0.0001, P_multiplier=100)
+    measured_positions, filtered_positions, errors, true_positions = run_filter(kalman_filter, test_target)
+
+    # Plot trajectories on top graph
+    ax1.scatter(*zip(*measured_positions), s=2, label='Измеренное положение')
+    ax1.plot(*zip(*filtered_positions), linewidth=.6, label='Отфильтрованное положение')
+    ax1.plot(*zip(*true_positions), linewidth=2, label='Истиное положение')
+    ax1.legend()
+
+    # Plot errors on bottom
+    ax2.plot(errors)
+
+    # Format output
+    ax2.set_yscale('log')
+    ax2.yaxis.set_major_formatter(FormatStrFormatter('%g'))
+    ax2.yaxis.set_minor_formatter(FormatStrFormatter('%g'))
+    ax2.tick_params(which='minor', labelsize=8)
+    ax2.grid(which='both')
+    ax2.legend()
+    plt.show()
---
+++ kalman_filters/kalman_filter_pytorch.py
@@ -0,0 +1,225 @@
+import numpy as np
+from math import *
+import torch
+from torch.autograd import Variable
+from torch import Tensor
+
+from kalman_filters.robot import Robot
+
+
+class KalmanFilterBase:
+    state_size = NotImplemented
+    measurement_size = NotImplemented
+
+    def __init__(self, test_target: Robot, measurement=None, R_multiplier=100, Q_multiplier=.001, P_multiplier=1000):
+        if measurement is None:
+            x_measurement, y_measurement = (0, 0)
+        else:
+            x_measurement, y_measurement = measurement
+
+        self.test_target = test_target
+
+        self.X = self._init_X(x_measurement, y_measurement)
+        self.I = np.matrix(np.identity(self.state_size))
+        self.H = self._get_H(self.X)
+        self.P = self._get_P(self.X, P_multiplier=P_multiplier)
+        self.R = self._get_R(self.X, R_multiplier=R_multiplier)
+
+        self.R_multiplier = R_multiplier
+        self.Q_multiplier = Q_multiplier
+        self.P_multiplier = P_multiplier
+
+    def get_position(self):
+        """Gets last estimated position"""
+        xy_estimate = self.X[0, 0], self.X[1, 0]
+        return xy_estimate
+
+    def _get_P(self, X, P_multiplier, dt=None):
+        P = self.I * P_multiplier
+        return P
+
+    def _get_R(self, X, R_multiplier, dt=None):
+        R = np.matrix(np.identity(self.measurement_size)) * R_multiplier
+        return R
+
+    def _init_X(self, *args, **kwargs):
+        raise NotImplementedError
+
+    def _get_F(self, X, dt=None):
+        raise NotImplementedError
+
+    def _get_H(self, X, dt=None):
+        raise NotImplementedError
+
+    def step(self, measurement, dt=1):
+        raise NotImplementedError
+
+    def get_prediction(self, steps=1, dt=1):
+        raise NotImplementedError
+
+
+class KalmanFilter(KalmanFilterBase):
+    def __init__(self, *args, **kwargs):
+        self.state_size = 6
+        self.measurement_size = 2
+
+        super().__init__(*args, **kwargs)
+
+    def _init_X(self, x_measurement, y_measurement):
+        return np.matrix([x_measurement, y_measurement, 0, 0, 0, 0]).T
+
+    def _get_F(self, X, dt=None):
+        F = np.matrix([
+            [1, 0, dt, 0, dt**2, 0],
+            [0, 1, 0, dt, 0, dt**2],
+            [0, 0, 1, 0, dt, 0],
+            [0, 0, 0, 1, 0, dt],
+            [0, 0, 0, 0, 1, 0],
+            [0, 0, 0, 0, 0, 1],
+        ])
+        return F
+
+    def _get_H(self, X, dt=None):
+        H = np.matrix([[1, 0, 0, 0, 0, 0],
+                       [0, 1, 0, 0, 0, 0]])
+        return H
+
+    def step(self, measurement, dt=1):
+        F = self._get_F(self.X, dt)
+        Q = F * F.T * self.Q_multiplier
+
+        self.X = F * self.X
+        self.P = F * self.P * F.T + Q
+
+        Z = np.matrix(measurement).T
+        Y = Z - self.H * self.X
+        S = self.H * self.P * self.H.T + self.R
+        K = self.P * self.H.T * np.linalg.pinv(S)
+        self.X = self.X + K * Y
+        self.P = (self.I - K * self.H) * self.P
+
+    def run_filter(self, steps=1000):
+        loss = 0
+
+        for step in range(steps):
+            measurement = self.test_target.sense()
+            self.step(measurement)
+
+        return loss
+
+    def get_prediction(self, steps=1, dt=1):
+        """Gets prediction or robot position after given number of steps"""
+        X = self.X
+
+        for step in range(steps):
+            F = self._get_F(X, dt)
+            X = F * X
+
+        xy_estimate = X[0, 0], X[1, 0]
+
+        return xy_estimate
+
+
+class ExtendedKalmanFilter(KalmanFilterBase):
+    prev_x = None
+    prev_y = None
+    prev_b = None
+
+    def __init__(self, *args, **kwargs):
+        self.state_size = 5
+        self.measurement_size = 3
+
+        super().__init__(*args, **kwargs)
+
+    def _unpack_X(self, X):
+        x = X[0, 0]
+        y = X[1, 0]
+        v = X[2, 0]
+        b = X[3, 0]
+        w = X[4, 0]
+        return x, y, v, b, w
+
+    def _f(self, X, dt):
+        x, y, v, b, w = self._unpack_X(X)
+        x, y, b = x + sin(b) * v * dt, y + cos(b) * v * dt, b + w * dt
+        return np.matrix((x, y, v, b, w)).T
+
+    def _init_X(self, x_measurement, y_measurement):
+        return np.matrix([x_measurement, y_measurement, 0.1, 0.1, 0.1]).T
+
+    def _get_F(self, X, dt=None):
+        x, y, v, b, w = self._unpack_X(X)
+        F = np.matrix(np.identity(5))
+
+        F[0, 2] = sin(b + w * dt) * dt
+        F[0, 3] = cos(b + w * dt) * v * dt
+        F[0, 4] = cos(b + w * dt) * v * dt**2
+
+        F[1, 2] = cos(b + w * dt) * dt
+        F[1, 3] = -sin(b + w * dt) * v * dt
+        F[1, 4] = -sin(b + w * dt) * v * dt**2
+
+        F[3, 4] = dt
+
+        return F
+
+    def _get_H(self, X, dt=None):
+        H = np.matrix([
+            [1, 0, 0, 0, 0],
+            [0, 1, 0, 0, 0],
+            [0, 0, 1, 0, 0],
+        ])
+        return H
+        # return self.I
+
+    def step(self, measurement, dt=1):
+        # Extract more data from measurements
+        current_x, current_y = measurement
+
+        if self.prev_x is not None and self.prev_y is not None:
+            dx = current_x - self.prev_x
+            dy = current_y - self.prev_y
+            current_dist = sqrt(dx**2 + dy**2)
+            v = current_dist / dt
+            b = atan2(dy, dx)
+        else:
+            v = 0
+            b = 0
+
+        if self.prev_b is not None:
+            db = b - self.prev_b
+            w = db / dt
+        else:
+            w = 0
+
+        self.prev_x = current_x
+        self.prev_y = current_y
+        self.prev_b = b
+
+        # measurement: measured x, measured y, linear velocity, current beta, angular velocity
+        # measurement = current_x, current_y, v, b, w
+        measurement = current_x, current_y, v
+
+        F = self._get_F(self.X, dt)
+        Q = F * F.T * self.Q_multiplier
+
+        self.X = self._f(self.X, dt)
+        self.P = F * self.P * F.T + Q
+
+        Z = np.matrix(measurement).T
+        Y = Z - self.H * self.X
+        S = self.H * self.P * self.H.T + self.R
+        K = self.P * self.H.T * np.linalg.pinv(S)
+        self.X = self.X + K * Y
+        self.P = (self.I - K * self.H) * self.P
+
+    def get_prediction(self, steps=1, dt=1):
+        """Gets prediction or robot position after given number of steps"""
+        X = self.X
+
+        for step in range(steps):
+            X = self._f(X, dt)
+
+        xy_estimate = X[0, 0], X[1, 0]
+
+        return xy_estimate
